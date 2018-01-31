14871552e521e7321160aeee4a9a9c577e1cf4dc
Chris Whong <chris@whong.org>
2018-01-31 00:23:10 -0500

    update import-pad script for better feedback

--- import-pad.sh
+++ import-pad.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# build nycpad docker image
-docker-compose build nycpad
-
 # get timestamp
 PELIAS_TIMESTAMP=`date +%s`
+
+# build nycpad docker image
+docker-compose build nycpad
 
 # set unique indexName using timestamp
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -18,23 +18,62 @@ docker-compose run --rm schema npm run create_index
 # download data
 docker-compose run --rm nycpad npm run download
 
+#get filename of downloaded csv
+FILENAME=$(jq .imports.nycpad.import[0].filename pelias.json | sed "s/\"//g")
+echo $FILENAME
+
+# get rowcount of downloaded CSV
+ROWCOUNT=$(wc -l < /tmp/nycpad/$FILENAME)
+ROWCOUNT=$(($ROWCOUNT - 1)) # subtract 1 for csv header row
+echo "Row count:" $ROWCOUNT
+
+curl -X POST \
+-H 'Content-type: application/json' \
+--data '{"text": "Starting NYCPAD import `pelias_'"$PELIAS_TIMESTAMP"'` from `'"$FILENAME"'` containing `'"$ROWCOUNT"'` addresses", "channel": "#labs-geocoder-api", "username": "GeoSearch Import Bot", "icon_emoji": ":robot_face:"}' \
+$SLACK_WEBHOOK_URL
+
 # import downloaded data
-SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL docker-compose run --rm nycpad npm start &&
+docker-compose run --rm nycpad npm start
 
-# clear all aliases
-curl -XPOST 'localhost:9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
-{
- "actions" : [
-    { "remove" : { "index" : "*", "alias" : "pelias" } }
- ]
-}
-'
+# get rowcount from DB
+curl -XPOST "http://localhost:9200/pelias_"$PELIAS_TIMESTAMP"/_refresh"
 
-# set alias
-curl -XPOST 'localhost:9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
-{
-    "actions" : [
-        { "add" : { "index" : "pelias_'"$PELIAS_TIMESTAMP"'", "alias" : "pelias" } }
-    ]
-}
-'
+ES_ROWCOUNT=$(curl "http://localhost:9200/pelias_"$PELIAS_TIMESTAMP"/address/_count" | jq .count)
+echo $(curl "http://localhost:9200/pelias_"$PELIAS_TIMESTAMP"/address/_count")
+echo $ES_ROWCOUNT
+
+echo "CSV Rowcount" $ROWCOUNT
+echo "ES Rowcount" $ES_ROWCOUNT
+
+if [ "$ROWCOUNT" -eq "$ES_ROWCOUNT" ];then
+  MESSAGE="Rowcounts Match, setting alias \`pelias\` on index \`pelias_"$PELIAS_TIMESTAMP"\`"
+
+  # clear all aliases
+  curl -XPOST 'localhost:9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
+  {
+   "actions" : [
+      { "remove" : { "index" : "*", "alias" : "pelias" } }
+   ]
+  }
+  '
+
+  # set alias
+  curl -XPOST 'localhost:9200/_aliases?pretty' -H 'Content-Type: application/json' -d'
+  {
+      "actions" : [
+          { "add" : { "index" : "pelias_'"$PELIAS_TIMESTAMP"'", "alias" : "pelias" } }
+      ]
+  }
+  '
+else
+  MESSAGE="I am sorry to inform you that the rowcounts did not match, something went wrong with this import..."
+fi
+
+echo $MESSAGE
+
+
+curl -X POST \
+-H 'Content-type: application/json' \
+--data '{"text": "'"$MESSAGE"'", "channel": "#labs-geocoder-api", "username": "GeoSearch Import Bot", "icon_emoji": ":robot_face:"}' \
+$SLACK_WEBHOOK_URL
+echo $MESSAGE
